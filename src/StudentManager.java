f4b692f39fa34704cd6e0d08980db161fcc5d638
WentingTang <3101802247@qq.com>
2023-12-21 01:36:39 +0800

    Implement CSV File Persistence for Student Data
    Description:
    This commit introduces significant enhancements to the StudentManager class, enabling CSV file-based persistence for student data:
    
    Data Loading: Implemented loadStudentsFromFile method to read student data from a CSV file at the program startup. This method parses each line of the CSV, creating Student objects and adding them to the student list.
    Data Saving: Added saveStudentsToFile method to write current student data back to the CSV file. This method is invoked before program exit to ensure all changes (additions, deletions, updates) are persisted.
    CSV Field Formatting: Included formatCsvField and unformatCsvField methods to handle CSV-specific formatting, ensuring proper handling of special characters and CSV format compliance.
    Error Handling: Added error handling to manage potential I/O issues during file reading and writing, with error messages for troubleshooting.
    These updates provide a robust solution for persisting student data across sessions, significantly enhancing the usability and reliability of the student management system.
    Some code about csv file error handling was learned from other team's team project.
    
    Co-Authored-By: NiuFeier <153836916+NiuFeier@users.noreply.github.com>
    Co-Authored-By: ZHUYunWen202383910034 <147827125+ZHUYunWen202383910034@users.noreply.github.com>
    Co-Authored-By: NiuFeier <153836916+NiuFeier@users.noreply.github.com>
    Co-Authored-By: ZHUYunWen202383910034 <147827125+ZHUYunWen202383910034@users.noreply.github.com>

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 class StudentManager {
     private List<Student> students;
+    private final String filePath = "data/students.csv"; // CSV file path
 
     public StudentManager() {
         students = new ArrayList<>();
+        loadStudentsFromFile(); // Load the student data in the constructor / 在构造函数中加载学生数据
     }
 
     public void addStudent(Student student) {
@@ -30,7 +37,7 @@ class StudentManager {
             student.setGender(newGender);
             System.out.println("Student information updated successfully！");
         } else {
-            System.out.println("The student ID was not found and the information cannot be updated.。");
+            System.out.println("The student ID was not found and the information cannot be updated.");
         }
     }
 
@@ -64,5 +71,50 @@ class StudentManager {
         }
         return foundStudents;
     }
+
+
+    private void loadStudentsFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                if (data.length >= 4) {
+                    students.add(new Student(unformatCsvField(data[0]),
+                            Integer.parseInt(data[1]),
+                            unformatCsvField(data[2]),
+                            unformatCsvField(data[3])));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Unable to load student data：" + e.getMessage());
+        }
+    }
+
+    public void saveStudentsToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Student student : students) {
+                String line = formatCsvField(student.getName()) + "," +
+                        student.getAge() + "," +
+                        formatCsvField(student.getGender()) + "," +
+                        formatCsvField(student.getStudentID());
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Unable to save student data：" + e.getMessage());
+        }
+    }
+
+    private String formatCsvField(String data) {
+        return "\"" + data.replace("\"", "\"\"") + "\"";
+    }
+
+    private String unformatCsvField(String data) {
+        if (data.startsWith("\"") && data.endsWith("\"")) {
+            data = data.substring(1, data.length() - 1); // Remove the double quotes / 移除首尾双引号
+        }
+        return data.replace("\"\"", "\""); // Converts two double quotes to one double quotation mark / 将两个双引号转换为一个双引号
+    }
 }
+
 
